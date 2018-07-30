a0792c57c11c85d856cb06d90c6386aadac66c73
karolinelemos <karolineclemos@hotmail.com>
2018-07-30 20:37:51 -0300

    initial config, header and menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ItemsListComponent } from './pages/items-list/items-list.component';
+import { AddItemComponent } from './pages/add-item/add-item.component';
+import { MenuComponent } from './menu/menu.component';
+
+import {ToolbarModule} from 'primeng/toolbar';
+import {ButtonModule} from 'primeng/button';
+import {MenuModule} from 'primeng/menu';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ItemsListComponent,
+    MenuComponent,
+    AddItemComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    BrowserAnimationsModule,
+    FormsModule, 
+    HttpClientModule, 
+    ToolbarModule,
+    ButtonModule,
+    MenuModule,
+    RouterModule.forRoot([
+      {path: '', component: ItemsListComponent},
+      {path: 'add-item', component: AddItemComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
